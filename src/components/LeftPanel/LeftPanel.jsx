e596798a646972ef01f9a3a642b78f00f1a9bb5c
Parth Tyagi <parthtyagicode@gmail.com>
2023-05-20 14:00:35 +0530

    left panel added

---
+++ src/components/LeftPanel/LeftPanel.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import './LeftPanel.css';
+import Logo from './logo.png';
+
+
+function LeftPanel() {
+    return (
+        <div class='leftPanelContainer'>
+            <div className="leftPanelUpper">
+                <div className="leftPanelTitle">
+                    <img id='leftPanelLogo' src={Logo} alt="logo" />
+                    Meteorica
+                </div>
+                <div className="leftPanelOptions">
+                    <span class="material-symbols-rounded">
+                        dashboard
+                    </span>
+                    Dashboard
+                </div>
+                <div className="leftPanelOptions">
+                    <span class="material-symbols-rounded">
+                        explore
+                    </span>
+                    Map
+                </div>
+                <div className="leftPanelOptions">
+                    <span class="material-symbols-rounded">
+                        share_location
+                    </span>
+                    Saved Location
+                </div>
+                <div className="leftPanelOptions">
+                    <span class="material-symbols-rounded">
+                        calendar_month
+                    </span>
+                    Calender
+                </div>
+            </div>
+
+            <div className="leftPanelLower">
+                <div>System</div>
+                <div className="leftPanelOptions">
+                    <span class="material-symbols-rounded">
+                        settings
+                    </span>
+                    Setting
+                </div>
+                <div className="leftPanelOptions">
+                    <span class="material-symbols-rounded">
+                        logout
+                    </span>
+                    Logout account
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LeftPanel;
